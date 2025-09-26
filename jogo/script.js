722000f475147ef66dba4043d430704b57478e7a
cauan coelho barreto <cauanbarbosa575@gmail.com>
2025-09-26 14:31:42 -0300

    Add files via upload

---
+++ jogo/script.js
@@ -0,0 +1,36 @@
+const mario = document.querySelector('.mario');
+const cano = document.querySelector('.cano');
+
+const jump =()=> {
+    mario.classList.add('jump');
+
+    setTimeout(()=>{
+
+        mario.classList.remove('jump');
+
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    
+    const canoPosition = cano.offsetLeft;
+    const marioPosition =+window.getComputedStyle(mario).bottom.replace ('px','');
+
+    if (canoPosition <= 120 && canoPosition > 0 && marioPosition <90) {
+        cano.style.animation = 'none'; 
+        cano.style.left = `${canoPosition}px`; 
+
+        mario.style.animation = 'none'; 
+        mario.style.bottom = `${marioPosition}px`; 
+
+        mario.src='./imagens/game-over.png';
+        mario.style.width='75px'
+        mario.style.marginleft='50px'
+
+        clearInterval(loop);
+    }
+}, 10);
+
+
+document.addEventListener("keydown",jump);
